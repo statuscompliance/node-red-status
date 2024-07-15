e0aba130a870da8f60aeaf582bb67d9fd59d16a9
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-07-15 12:10:27 +0200

    feat: Add bluejay status param

--- nodes/bpm/implies.js
+++ nodes/bpm/implies.js
@@ -6,6 +6,7 @@ module.exports = function (RED) {
         let payloads = [];
 
         node.on("input", function (msg) {
+            var expectedStatus = msg.req.query.Status;
             if (typeof msg.payload === "boolean") {
                 payloads.push(msg.payload);
             }
@@ -13,9 +14,17 @@ module.exports = function (RED) {
                 let A = payloads[0];
                 let B = payloads[1];
                 let implies = !A || B;
-                msg.payload = { result: implies };
-                payloads = [];
-                node.send(msg);
+                if (implies === true && expectedStatus === "true") {
+                    msg.payload = { result: implies };
+                    payloads = [];
+                    node.send(msg);
+                } else if (expectedStatus === "false") {
+                    msg.payload = { result: implies };
+                    payloads = [];
+                    node.send(msg);
+                } else {
+                    payloads = [];
+                }
             }
         });
     }
