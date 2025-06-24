5474f50be8a9955620043f0d49247870e6405031
tirirote <josefranciscoaguzabenitez@gmail.com>
2025-06-24 15:11:29 +0200

    chore: refactor of evidence handling

---
+++ utils/common.js
@@ -0,0 +1,39 @@
+export function existSection(msg, storeEvidences) {
+    let data = msg.payload.result;
+    let section = msg.req?.body?.section ?? config.section;
+    const result = data.includes(section).toString();
+    msg.payload.result = result;
+    const evidence = {
+        section: section,
+        data: data,
+    }
+    addEvidence(msg, "Section", evidence, result, storeEvidences);
+    node.send(msg);
+}
+
+export function extractTextFromPDF(url, node, msg, storeEvidences) {
+    pdfParse(url)
+        .then((data) => {
+            msg.payload.result = data.text;
+            existSection(msg);
+        })
+        .catch((error) => {
+            node.error(
+                "Error extracting text from PDF: " + error.message
+            );
+            msg.payload.result = false;
+            addEvidence(msg, "PDF", "Error extracting text from PDF", false, storeEvidences);
+            node.send(msg);
+        });
+}
+
+export function addEvidence(msg, key, value, result, storeEvidences) {
+    if (storeEvidences) {
+        msg.payload.evidences.push({
+            id: uuidv4(),
+            key,
+            value,
+            result
+        });
+    }
+}
